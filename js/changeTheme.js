e28f2230108ce2bd329eec5a20ebcf31d1d59f9f
Kowsihan-sk <kowsihan2sk@gmail.com>
2021-07-14 02:05:06 +0530

    uses theme previously selected by user

--- js/changeTheme.js
+++ js/changeTheme.js
@@ -4,6 +4,9 @@ function setTheme(themeName) {
 }
 
 (function () {
-    localStorage.setItem('theme', 'theme-1');
-    document.documentElement.className = 'theme-1';
+    if (localStorage.getItem('theme') === 'theme-1') {
+        setTheme('theme-1');
+    } else if (localStorage.getItem('theme') === 'theme-2') {
+        setTheme('theme-2')
+    } else setTheme('theme-3');
 })();
